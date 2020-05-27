fc23a4035ae9786aa39a9fec36d69573c9a72e07
Antonio Russo <arusso@sandia.gov>
2020-05-26 18:37:10 -0600

    More by-hand changes

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -4,7 +4,7 @@ import unittest
 class SmokeTester(unittest.TestCase):
     def test_smoketest(self):
         # Simple test to confirm that we can import both the
-        # man jaqal-pup packages, and the extras here.
+        # man JaqalPaq packages, and the extras here.
         import jaqalpaq.core
         import jaqalpaq
         import jaqalpaq.transpilers
