861a690a6b9d6ba467b609a1dd636dc9aaf9111d
Antonio Russo <arusso@sandia.gov>
2020-08-05 19:19:39 -0600

    tests: install testing dependencies
    Signed-off-by: Antonio Russo <arusso@sandia.gov>

--- setup.py
+++ setup.py
@@ -23,8 +23,15 @@ setup(
         ]
     ),
     package_dir={"": "."},
-    install_requires=["JaqalPaq"],
-    extras_requires={"tests": ["pytest"],},
+    install_requires=[f"JaqalPaq=={version}"],
+    extras_require={
+        "tests": ["pytest"],
+        "qiskit": ["qiskit>=0.19.0,<0.20.0", "qscout-gatemodels"],
+        "pyquil": ["pyquil>=2.21.0,<3.0.0", "qscout-gatemodels"],
+        "cirq": ["cirq>=0.8.2,<0.9.0", "qscout-gatemodels"],
+        "projectq": ["projectq>=0.5.1,<0.6.0", "qscout-gatemodels"],
+        "pytket": ["pytket>=0.5.6,<0.6.0", "qscout-gatemodels"],
+    },
     python_requires=">=3.6.5",
     platforms=["any"],
     url="https://qscout.sandia.gov",
